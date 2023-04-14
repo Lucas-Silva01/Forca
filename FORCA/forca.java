7a404052b2086dc614f2ceedf2a7a8958893a286
Lucas Silva <115842704+MAIN-ASHE-OW2@users.noreply.github.com>
2023-04-14 20:14:26 -0300

    Add files via upload

---
+++ FORCA/forca.java
@@ -0,0 +1,59 @@
+import javax.swing.JOptionPane;
+
+public class forca{
+    // Lista de palavras-chave para o jogo
+    private static String[] palavras = {"maça", "pera", "banana", "uva"};
+        public static void main(String[] args) {
+        String nome = JOptionPane.showInputDialog("Digite seu nome:");
+        String email = JOptionPane.showInputDialog("Digite seu email:");
+        
+        JOptionPane.showMessageDialog(null, "Bem-vindo(a) ao Jogo da Forca, " + nome + " (" + email + ")!");
+        
+        // Seleciona uma palavra aleatória da lista de palavras-chave
+        String palavra = palavras[(int) (Math.random() * palavras.length)];
+        
+        // Cria uma string com a palavra em branco
+        StringBuilder palavraEmBranco = new StringBuilder();
+        for (int i = 0; i < palavra.length(); i++) {
+            palavraEmBranco.append("_");
+        }
+        
+        int tentativas = 7;
+        while (tentativas > 0) {
+            // Mostra a palavra em branco e as tentativas restantes
+            JOptionPane.showMessageDialog(null, "Palavra: " + palavraEmBranco.toString() + "\nTentativas restantes: " + tentativas);
+            
+            // Solicita a letra do usuário
+            String letra = JOptionPane.showInputDialog("Digite uma letra:");
+            
+            // Verifica se a letra está na palavra
+            boolean acertou = false;
+            for (int i = 0; i < palavra.length(); i++) {
+                if (palavra.charAt(i) == letra.charAt(0)) {
+                    palavraEmBranco.setCharAt(i, letra.charAt(0));
+                    acertou = true;
+                }
+            }
+            
+            // Se o usuário acertou, mostra uma mensagem de parabéns
+            if (acertou) {
+                JOptionPane.showMessageDialog(null, "Parabéns, você acertou!");
+            } else {
+                // Se o usuário errou, decrementa as tentativas restantes e mostra uma mensagem de erro
+                tentativas--;
+                JOptionPane.showMessageDialog(null, "Ops, você errou.");
+            }
+            
+            // Verifica se o usuário acertou toda a palavra
+            if (palavraEmBranco.toString().equals(palavra)) {
+                JOptionPane.showMessageDialog(null, "Parabéns, você acertou a palavra " + palavra + "!");
+                break;
+            }
+        }
+        
+        // Se o usuário não acertou a palavra, mostra a palavra correta
+        if (tentativas == 0) {
+            JOptionPane.showMessageDialog(null, "Que pena, você não conseguiu acertar a palavra " + palavra + ".");
+        }
+    }
+}
